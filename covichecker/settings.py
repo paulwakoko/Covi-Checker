c077c7fd7071ce1eb6c1ef9f118d8a1378722f47
SoftCysec <softengdedan@gmail.com>
2022-12-01 01:31:03 +0300

    Database Modified

--- covichecker/settings.py
+++ covichecker/settings.py
@@ -78,7 +78,11 @@ WSGI_APPLICATION = 'covichecker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'covi-checker',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PORT': '3306',
+        'PASSWORD': '',
     }
 }
 
