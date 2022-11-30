54293e1eefd2853037015a4c4d22bfd8dd6ad4e5
SoftCysec <softengdedan@gmail.com>
2022-12-01 01:53:05 +0300

    Database Modified

--- covichecker/settings.py
+++ covichecker/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql 
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'covichecker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'covi-checker',
         'HOST': 'localhost',
         'USER': 'root',
